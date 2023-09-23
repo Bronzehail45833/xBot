e4bba7fd55fd36bffcb6c4fcc00f1f724ad845fd
Animeluvr73 <52725641+Bronzehail45833@users.noreply.github.com>
2023-09-23 11:28:16 -0500

    Add files via upload

---
+++ Checker_bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from GetGamertag import Checker
+
+Token = ''
+Channel_id = 1141747340743557222
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'logged in')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Gamertags"))
+
+
+@bot.command()
+async def check(ctx, gamertag=None):
+    gt_Checker = Checker(gamertag)
+    gt_Checker.GetData(gamertag)
+    if gamertag is None:
+        await ctx.send("Please supply a gamertag to be checked. \nUsage: check gamertag")
+        return
+    
+    await ctx.send(f'The gamertag {gamertag} {gt_Checker.GetAvailabiltity(gamertag)}!')
+
+bot.run(Token)
